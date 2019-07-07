9c575c93367dad5f2af2cb8ee330f04ac6c90bc9
v-kazantsev <vvs201@mail.ru>
2019-07-07 18:36:09 +0700

    add custom router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch, RouteComponentProps } from 'react-router-dom'
 import { About, Items, Menu, NotFound } from 'pages'
+import { routes } from './routes'
 import './App.css'
+
+const mapPageToComponent = (page: string): React.FC<RouteComponentProps> => {
+  switch (page) {
+    case 'menu': return Menu
+    case 'items': return Items
+    case 'about': return About
+    default: return NotFound
+  }
+}
 
 const App: React.FC = () => {
   return (
     <HashRouter>
       <Switch>
-        <Route exact path='/' component={Menu} />
-        <Route path='/items' component={Items} />
-        <Route path='/about' component={About} />
+        {Object.entries(routes).map(entry =>(
+          <Route path={`/${entry[0]}`} component={mapPageToComponent(entry[1])} />
+        ))}
         <Route component={NotFound} />
       </Switch>
     </HashRouter>
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+interface Nav {
+  [route: string]: string
+}
+
+export const routes: Nav = {
+  'menu': 'Menu',
+  'items': 'ItemsPage',
+  'about': 'AboutPage',
+}
